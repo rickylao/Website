85a485dd21e34b06ff04e4c09908f952da079f7a
Ricky Lao <rickylao@uri.edu>
2024-08-11 19:03:33 -0400

    wow a lot of progress

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,11 @@
-const cool = 3.14;
-console.log(cool);
+const colors = Object.freeze({
+    kombuGreen: 0x31493c,
+    ashGrey: 0xbdbea9,
+    richBlack: 0x001a23,
+    trueBlue: 0x3066be,
+    theEmerald: 0x45d58b
+});
+
+let numbers = [1, 5, 2, 6, 3];
+numbers.sort();
+console.log(numbers);
